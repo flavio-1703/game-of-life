d5453e6f9c4f763410733c43c43eaf1f8dde7007
flavio-1703 <eleziflavio@gmail.com>
2021-11-14 20:39:38 +0100

    init

---
+++ main.c
@@ -0,0 +1,98 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+#define WIDTH  10
+#define HEIGHT 10
+
+#define DEAD_ASCII "\u25a1"
+#define ALIVE_ASCII "\u25A0"
+#define DEAD  0
+#define ALIVE 1
+
+// FUNCS
+void delay(uint32_t secs);
+void print_array(uint32_t array[][HEIGHT]);
+void game(uint32_t array[][HEIGHT]);
+uint32_t count_neighbors(uint32_t array[][HEIGHT], uint32_t x, uint32_t y);
+
+int main(void) {
+  uint32_t array[WIDTH][HEIGHT] = {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+  };
+  
+  while (1) {
+    system("clear");
+    print_array(array);
+    game(array);
+    delay(1);
+  }  
+
+  return 0;
+}
+
+// DELAY TIME
+void delay(uint32_t secs) {
+  uint32_t ret_time = time(0) + secs;
+  while (time(0) < ret_time);  
+}
+
+// PRINT ARRAY
+void print_array(uint32_t array[][HEIGHT]) {
+  for (uint32_t i = 0; i < WIDTH; i++){
+    for (uint32_t j = 0; j < HEIGHT; j++) {
+      if (array[i][j] == ALIVE)
+        printf("%s", ALIVE_ASCII);
+      else
+        printf("%s", DEAD_ASCII);
+      printf("%s", "  ");
+    }
+    printf("%s", "\n");
+  }   
+}
+
+// RETURNS THE NUMBER OF NEIGHBORS
+uint32_t count_neighbors(uint32_t array[][HEIGHT], uint32_t x, uint32_t y) {
+  uint32_t sum = 0;
+  for (int32_t i = -1; i < 2; i++){
+    for (int32_t j = -1; j < 2; j++) {
+      uint32_t col = (x + i + WIDTH) % WIDTH;
+      uint32_t row = (y + j + HEIGHT) % HEIGHT;
+      sum += array[col][row];
+    }
+  } 
+  sum -= array[x][y];
+  return sum;
+}
+
+// APPLY GAME RULES
+void game(uint32_t array[][HEIGHT]) {
+  uint32_t next[WIDTH][HEIGHT];
+  //COMPUTE NEXT GEN
+  for (uint32_t i = 0; i < WIDTH; i++){
+    for (uint32_t j = 0; j < HEIGHT; j++) {
+      uint32_t state = array[i][j];
+      //CHECK IF THE CELL IS ON THE EDGE 
+      uint32_t neighbors = count_neighbors(array, i, j);
+      //RULES
+      if (state == DEAD && neighbors == 3) 
+        next[i][j] = ALIVE;
+      else if (state == ALIVE && (neighbors < 2 || neighbors > 3)) 
+        next[i][j] = DEAD;
+      else 
+        next[i][j] = state;
+    }
+  } 
+  memcpy(array, next, HEIGHT * WIDTH * sizeof(uint32_t));
+}
+
