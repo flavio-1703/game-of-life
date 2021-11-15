a52b15370b00c932613ab49e00736fd8f5eeeee7
flavio-1703 <eleziflavio@gmail.com>
2021-11-15 12:57:24 +0100

    File grid and new time delay

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 #include <time.h>
 #include <string.h>
 
-#define WIDTH  10
-#define HEIGHT 10
+#define WIDTH  40
+#define HEIGHT 40
 
 #define DEAD_ASCII  "\u25a1"
 #define ALIVE_ASCII "\u25A0"
@@ -12,59 +12,77 @@
 #define ALIVE 1
 
 // FUNCS
-void delay(uint32_t secs);
-void print_grid(uint32_t grid[][HEIGHT]);
-void game(uint32_t grid[][HEIGHT]);
-uint32_t count_neighbors(uint32_t grid[][HEIGHT], uint32_t x, uint32_t y);
+void delay( uint32_t milliseconds );
+void print_grid( uint8_t grid[][HEIGHT] );
+void game( uint8_t grid[][HEIGHT] );
+uint8_t count_neighbors( uint8_t grid[][HEIGHT], uint32_t x, uint32_t y );
+static void load_grid_from_file();
 
-int main(void) {
+int main( void ) {
   // INIT GRID
-  uint32_t grid[WIDTH][HEIGHT] = {
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-  };
-  
+  uint8_t grid[WIDTH][HEIGHT];
+  load_grid_from_file(grid, "grid.txt");
+
   while (1) {
     system("clear");
     print_grid(grid);
     game(grid);
-    delay(1);
+    delay(100);
   }  
 
   return 0;
 }
 
+// LOAD GRID FROM FILE
+static void load_grid_from_file( uint8_t grid[][HEIGHT],  const char * filename ) {
+  FILE * file;
+  char *line, *number;
+  uint32_t i = 0, j;
+  file = fopen(filename, "r");
+  while (!feof(file)) {
+    j = 0;
+    fscanf(file, "%s", line);
+    number = strtok(line, ",");
+    
+    while(number != NULL) {
+      grid[i][j] = atoi(number);
+      j++;
+      number = strtok(NULL, ","); 
+    }
+    i++;
+  }
+  fclose(file);
+}
+
 // DELAY TIME
-void delay(uint32_t secs) {
-  uint32_t ret_time = time(0) + secs;
-  while (time(0) < ret_time);  
+void delay( uint32_t milliseconds )
+{
+    uint64_t pause;
+    clock_t now,then;
+
+    pause = milliseconds*(CLOCKS_PER_SEC/1000);
+    now = then = clock();
+    while( (now-then) < pause )
+        now = clock();
 }
 
 // PRINT ARRAY
-void print_grid(uint32_t grid[][HEIGHT]) {
+void print_grid( uint8_t grid[][HEIGHT] ) {
   for (uint32_t i = 0; i < WIDTH; i++){
     for (uint32_t j = 0; j < HEIGHT; j++) {
       if (grid[i][j] == ALIVE)
         printf("%s", ALIVE_ASCII);
       else
         printf("%s", DEAD_ASCII);
-      printf("%s", "  ");
+      printf("%s", " ");
     }
     printf("%s", "\n");
   }   
 }
 
 // RETURNS THE NUMBER OF NEIGHBORS
-uint32_t count_neighbors(uint32_t grid[][HEIGHT], uint32_t x, uint32_t y) {
-  uint32_t sum = 0;
+uint8_t count_neighbors( uint8_t grid[][HEIGHT], uint32_t x, uint32_t y ) {
+  uint8_t sum = 0;
   for (int32_t i = -1; i < 2; i++){
     for (int32_t j = -1; j < 2; j++) {
       uint32_t col = (x + i + WIDTH) % WIDTH;
@@ -77,14 +95,14 @@ uint32_t count_neighbors(uint32_t grid[][HEIGHT], uint32_t x, uint32_t y) {
 }
 
 // APPLY GAME RULES
-void game(uint32_t grid[][HEIGHT]) {
-  uint32_t next[WIDTH][HEIGHT];
+void game( uint8_t grid[][HEIGHT] ) {
+  uint8_t next[WIDTH][HEIGHT];
   //COMPUTE NEXT GEN
   for (uint32_t i = 0; i < WIDTH; i++){
     for (uint32_t j = 0; j < HEIGHT; j++) {
-      uint32_t state = grid[i][j];
+      uint8_t state = grid[i][j];
       //CHECK IF THE CELL IS ON THE EDGE 
-      uint32_t neighbors = count_neighbors(grid, i, j);
+      uint8_t neighbors = count_neighbors(grid, i, j);
       //RULES
       if (state == DEAD && neighbors == 3) 
         next[i][j] = ALIVE;
@@ -94,6 +112,6 @@ void game(uint32_t grid[][HEIGHT]) {
         next[i][j] = state;
     }
   } 
-  memcpy(grid, next, HEIGHT * WIDTH * sizeof(uint32_t));
+  memcpy(grid, next, HEIGHT * WIDTH * sizeof(uint8_t));
 }
 
